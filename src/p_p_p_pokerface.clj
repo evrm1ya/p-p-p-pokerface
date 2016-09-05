63e9f01d814bf8e3055e2d58fda2f33ab62895c2
evrm1ya <emvermilyea15@gmail.com>
2016-09-05 13:01:06 -0500

    finish exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,83 @@
 (ns p-p-p-pokerface)
 
+(def replacements {\T 10 \J 11 \Q 12 \K 13 \A 14})
+
 (defn rank [card]
-  nil)
+  (let [first-char (first card)]
+    (if (Character/isDigit first-char)
+      (Integer/valueOf (str first-char))
+      (get replacements first-char))))
 
 (defn suit [card]
-  nil)
+  (str (second card)))
 
+;; HELPERS
+(defn ranks-of-hand [hand]
+  (map rank hand))
+
+(defn frequencies-of? [ranks-or-suits hand]
+  (set (vals (frequencies (ranks-or-suits hand)))))
+
+(defn contains-?-of-a-kind [hand amount]
+  (contains? (frequencies-of? ranks-of-hand hand) amount))
+
+(defn suits-of-hand [hand]
+  (map suit hand))
+
+(defn contains-?-of-a-suit [hand amount]
+  (contains? (frequencies-of? suits-of-hand hand) amount))
+
+(defn sorted-ranks [hand]
+  (let [ranks (ranks-of-hand hand)
+        lowest-rank (apply min ranks)]
+    (if (and (contains? (set ranks) 14) (< lowest-rank 10))
+      (vec (sort (replace {14 1} ranks)))
+      (vec (sort ranks)))))
+
+;; EXERCISES
 (defn pair? [hand]
-  nil)
+  (contains-?-of-a-kind hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains-?-of-a-kind hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains-?-of-a-kind hand 4))
 
 (defn flush? [hand]
-  nil)
+  (contains-?-of-a-suit hand 5))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [set-of-ranks (set (ranks-of-hand hand))]
+    (and 
+      (not (three-of-a-kind? hand))
+      (or (= 3 (count set-of-ranks))
+          (four-of-a-kind? hand)))))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted (sorted-ranks hand)
+        lowest-rank (first sorted)]
+    (= sorted (range lowest-rank (+ lowest-rank 5)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true)
+
+(def checkers #{[high-card? 0] [pair? 1]
+                [two-pairs? 2] [three-of-a-kind? 3]
+                [straight? 4] [flush? 5]
+                [full-house? 6] [four-of-a-kind? 7]
+                [straight-flush? 8]})
 
 (defn value [hand]
-  nil)
+  (let [checks (filter #((first %) hand) checkers)
+        values (map #(second %) checks)]
+    (apply max values)))
+
+
+
